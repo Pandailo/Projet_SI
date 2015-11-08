f7ba18efa9953805ebbb38821808a21da8d4f4ca
Pandailo <veron.yann@gmail.com>
2015-11-08 10:34:07 +0100

    cube

---
+++ cube.cpp
@@ -0,0 +1,223 @@
+/********************************************************/
+/*                     cube.cpp                                                 */
+/********************************************************/
+/*                Affiche a l'ecran un cube en 3D                      */
+/********************************************************/
+
+/* inclusion des fichiers d'en-tete freeglut */
+
+#ifdef __APPLE__
+#include <GLUT/glut.h> /* Pour Mac OS X */
+#else
+#include <GL/glut.h>   /* Pour les autres systemes */
+#endif 
+#include <cstdlib>
+
+/*class Point*/
+class Point{
+    public :
+	//coordonnées x, y et z du point
+	double x;
+	double y;
+	double z;
+	// couleur r, v et b du point
+	float r;
+	float g;
+	float b;
+};
+  
+
+
+char presse;
+int anglex,angley,x,y,xold,yold;
+
+/* Prototype des fonctions */
+void affichage();
+void clavier(unsigned char touche,int x,int y);
+void reshape(int x,int y);
+void idle();
+void mouse(int bouton,int etat,int x,int y);
+void mousemotion(int x,int y);
+void Tete();
+void Corps();
+int main(int argc,char **argv)
+{
+  /* initialisation de glut et creation
+     de la fenetre */
+  glutInit(&argc,argv);
+  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+  glutInitWindowPosition(200,200);
+  glutInitWindowSize(500,500);
+  glutCreateWindow("cube");
+
+  /* Initialisation d'OpenGL */
+  glClearColor(0.0,0.0,0.0,0.0);
+  glColor3f(1.0,1.0,1.0);
+  glPointSize(2.0);
+  glEnable(GL_DEPTH_TEST);
+
+  /* enregistrement des fonctions de rappel */
+  glutDisplayFunc(affichage);
+  glutKeyboardFunc(clavier);
+  glutReshapeFunc(reshape);
+  glutMouseFunc(mouse);
+  glutMotionFunc(mousemotion);
+
+  /* Entree dans la boucle principale glut */
+  glutMainLoop();
+  return 0;
+}
+
+
+void affichage()
+{
+  int i,j;
+  /* effacement de l'image avec la couleur de fond */
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+glShadeModel(GL_SMOOTH);
+  
+  glLoadIdentity();
+  glRotatef(angley,1.0,0.0,0.0);
+  glRotatef(anglex,0.0,1.0,0.0);
+  Tete();
+  Corps();
+    
+    //Repère
+    //axe x en rouge
+    glBegin(GL_LINES);
+        glColor3f(1.0,0.0,0.0);
+    	glVertex3f(0, 0,0.0);
+    	glVertex3f(1, 0,0.0);
+    glEnd();
+    //axe des y en vert
+    glBegin(GL_LINES);
+    	glColor3f(0.0,1.0,0.0);
+    	glVertex3f(0, 0,0.0);
+    	glVertex3f(0, 1,0.0);
+    glEnd();
+    //axe des z en bleu
+    glBegin(GL_LINES);
+    	glColor3f(0.0,0.0,1.0);
+    	glVertex3f(0, 0,0.0);
+    	glVertex3f(0, 0,1.0);
+    glEnd();
+
+  glFlush();
+  
+  //On echange les buffers 
+  glutSwapBuffers();
+}
+
+void clavier(unsigned char touche,int x,int y)
+{
+  switch (touche)
+    {
+    case 'p': /* affichage du carre plein */
+      glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+      glutPostRedisplay();
+      break;
+    case 'f': /* affichage en mode fil de fer */
+      glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+      glutPostRedisplay();
+      break;
+    case 's' : /* Affichage en mode sommets seuls */
+      glPolygonMode(GL_FRONT_AND_BACK,GL_POINT);
+      glutPostRedisplay();
+      break;
+    case 'd':
+      glEnable(GL_DEPTH_TEST);
+      glutPostRedisplay();
+      break;
+    case 'D':
+      glDisable(GL_DEPTH_TEST);
+      glutPostRedisplay();
+      break;
+    case 'q' : /*la touche 'q' permet de quitter le programme */
+      exit(0);
+    }
+}
+
+void reshape(int x,int y)
+{
+  if (x<y)
+    glViewport(0,(y-x)/2,x,x);
+  else 
+    glViewport((x-y)/2,0,y,y);
+}
+//Dessin Tête robot
+void Tete()
+{
+	//crâne
+	glPushMatrix();
+	glTranslatef(0, 0.5, 0);
+	glColor3f(1.0, 1.0, 1.0);
+	glutSolidSphere(0.1, 20, 20);
+	glPopMatrix();
+	//Nez
+	glPushMatrix();
+	glTranslatef(0, 0.5, .05);
+	glRotatef(60,0, 0, 1);
+	glRotatef(-30, 1, 0, 0);
+	glColor3f(1, 0.4, 0);
+	glutSolidCone(0.05, 0.15, 20, 20);
+	glPopMatrix();
+	//YEUX
+	//1
+	glPushMatrix();
+	glColor3f(0, 0, 0);
+	glTranslatef(0.015, .57, 0.055);
+	glutSolidSphere(0.025, 20, 20);
+	glPopMatrix();
+	//2
+	glPushMatrix();
+	glColor3f(0, 0, 0);
+	glTranslatef(-0.06, .57, 0.03);
+	glutSolidSphere(0.025, 20, 20);
+	glPopMatrix();
+}
+//Dessin corps robot
+void Corps()
+{
+	//Tronc
+	glPushMatrix();
+	glColor3f(1, 1, 1);
+	glTranslatef(0, 0.2, 0);
+	glutSolidSphere(0.22, 20, 20);
+	glPopMatrix();
+	//Bras1
+	//TODO a mettre un cylindre a facettes.
+	//Bras2
+	//TODO A copier code avant
+}
+//Dessin roues
+
+void mouse(int button, int state,int x,int y)
+{
+  /* si on appuie sur le bouton gauche */
+  if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) 
+  {
+    presse = 1; /* le booleen presse passe a 1 (vrai) */
+    xold = x; /* on sauvegarde la position de la souris */
+    yold=y;
+  }
+  /* si on relache le bouton gauche */
+  if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) 
+    presse=0; /* le booleen presse passe a 0 (faux) */
+}
+
+void mousemotion(int x,int y)
+  {
+    if (presse) /* si le bouton gauche est presse */
+    {
+      /* on modifie les angles de rotation de l'objet
+	 en fonction de la position actuelle de la souris et de la derniere
+	 position sauvegardee */
+      anglex=anglex+(x-xold); 
+      angley=angley+(y-yold);
+      glutPostRedisplay(); /* on demande un rafraichissement de l'affichage */
+    }
+    
+    xold=x; /* sauvegarde des valeurs courante de le position de la souris */
+    yold=y;
+  }
+
